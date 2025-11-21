86e332149bcd54017437de68af7e0023b594346d
shadowforce78 <planque.adam@gmail.com>
2025-11-21 14:45:35 +0100

    Mise à jour de la configuration du transporteur d'email pour sécuriser la connexion

--- routes/index.js
+++ routes/index.js
@@ -105,7 +105,7 @@ router.post('/contact', [
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
       port: process.env.EMAIL_PORT,
-      secure: false,
+      secure: true, // true pour le port 465, false pour les autres ports
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
